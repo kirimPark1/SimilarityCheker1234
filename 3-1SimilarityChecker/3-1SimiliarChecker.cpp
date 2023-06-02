e85e308de1da89978a97e2ce31a0394950e7dd3b
kirim <kirim.park@naver.com>
2023-06-02 16:18:01 +0900

    implementation for alphaChecker

--- 3-1SimilarityChecker/3-1SimiliarChecker.cpp
+++ 3-1SimilarityChecker/3-1SimiliarChecker.cpp
@@ -1,4 +1,10 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <stdexcept>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 class LengthChecker
@@ -25,5 +31,56 @@ public:
 			}
 			return ((1 - (static_cast<double>(longStr - shortStr) / shortStr)) * 60);
 		}
+	}
+};
+class AlphaChecker
+{
+public:
+	int getSameAlphabetScore(const string &str1, const string &str2)
+	{
+		set<char> sortedStr1 = getAlphaSetfromString(str1);
+		set<char> sortedStr2 = getAlphaSetfromString(str2);
+		set<char> totalAlphaSet;
+		merge(sortedStr1.begin(), sortedStr1.end(), sortedStr2.begin(), sortedStr2.end(), std::inserter(totalAlphaSet, totalAlphaSet.begin()));
+		int samecnt = 0;
+		for (char curChar : totalAlphaSet)
+		{
+			if (isExistString(str1, curChar)
+				&& isExistString(str2, curChar)) samecnt += 1;
+		}
+		return static_cast<double>(samecnt) / totalAlphaSet.size() * 40;
+		return totalAlphaSet.size();
+	}
+	int check(const string& str1, const string& str2)
+	{
+		if (isLowerCase(str1) || isLowerCase(str2))
+		{
+			throw std::invalid_argument(("we support uppercase"));
+		}
+		return getSameAlphabetScore(str1, str2);
+	}
+
+	bool isLowerCase(const string& str1)
+	{
+		for (int i = 0; i < str1.length(); i++)
+		{
+			if (str1.at(i) < 'A' || str1.at(i) > 'Z') return true;
+		}
+		return false;
+	}
+
+	set<char> getAlphaSetfromString(const string& str1)
+	{
+		set<char> alphaList;
+		for (int i = 0; i < str1.length(); i++)
+		{
+			alphaList.insert(str1.at(i));
+		}
+		return alphaList;
+	}
+
+	bool isExistString(const string& str1, char curChar)
+	{
+		return str1.find(curChar) <= str1.length();
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -26,3 +26,31 @@ TEST(TestCaseName, LengthCheckDiffPartial2) {
 	std::string str2 = "AAA";
 	EXPECT_EQ(30, lengthChecker.check(str1, str2));
 }
+
+TEST(TestCaseName, sameAlphaCheck) {
+	AlphaChecker alphaChecker;
+	std::string str1 = "ASD";
+	std::string str2 = "DSA";
+	EXPECT_EQ(40, alphaChecker.check(str1, str2));
+}
+
+TEST(TestCaseName, diffAlphaCheck) {
+	AlphaChecker alphaChecker;
+	std::string str1 = "A";
+	std::string str2 = "BB";
+	EXPECT_EQ(0, alphaChecker.check(str1, str2));
+}
+
+TEST(TestCaseName, lowercaseAlphaCheck) {
+	AlphaChecker alphaChecker;
+	std::string str1 = "asd";
+	std::string str2 = "DSA";
+	EXPECT_THROW(alphaChecker.check(str1, str2), std::invalid_argument);
+}
+
+TEST(TestCaseName, AlphaDiffCheck) {
+	AlphaChecker alphaChecker;
+	std::string str1 = "AA";
+	std::string str2 = "AAE";
+	EXPECT_EQ(20, alphaChecker.check(str1, str2));
+}
