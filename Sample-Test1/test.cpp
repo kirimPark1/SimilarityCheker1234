509c918b8e3e7539639fe89a2d716caab0a5bb43
kirim <kirim.park@naver.com>
2023-06-02 13:50:25 +0900

    Revert "implementation for length"
    This reverts commit 73e6cf87bf5fb08fc3c77e18adb42a905eda5054.

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,28 +1,6 @@
 #include "pch.h"
-#include "../3-1SimilarityChecker/3-1SimiliarChecker.cpp"
-using namespace std;
 
-TEST(TestCaseName, sameLengthCheck) {
-	LengthChecker lengthChecker;
-	std::string str1 = "ASD";
-	std::string str2 = "DSA";
-	EXPECT_EQ(60, lengthChecker.check(str1, str2));
-}
-TEST(TestCaseName, LengthCheckDiff2X) {
-	LengthChecker lengthChecker;
-	std::string str1 = "A";
-	std::string str2 = "BB";
-	EXPECT_EQ(0, lengthChecker.check(str1, str2));
-}
-TEST(TestCaseName, LengthCheckDiffPartial1) {
-	LengthChecker lengthChecker;
-	std::string str1 = "AAABB";
-	std::string str2 = "BAA";
-	EXPECT_EQ(20, lengthChecker.check(str1, str2));
-}
-TEST(TestCaseName, LengthCheckDiffPartial2) {
-	LengthChecker lengthChecker;
-	std::string str1 = "AA";
-	std::string str2 = "AAA";
-	EXPECT_EQ(30, lengthChecker.check(str1, str2));
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
 }
